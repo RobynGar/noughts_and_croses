db4d0a1a19c2e0a60163acc6b2140b0918d2844f
Shirhan <shirhan25@hotmail.com>
2022-03-10 13:16:59 +0000

    logic for swithing symbols js

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-startGame()
 
 
-const startGameButton = document.querySelector
+let currentPlayer= "X";
+
+
+
+//get the grid element
+const gridElements = document.getElementsByClassName("box");
+
+//loop through each box, avoid writing out each box
+for (i = 0; i < gridElements.length; i++){
+    //each box is the element in the array
+   let box = gridElements[i];
+   //event listener is listening out for the user to click on each box and 
+    box.addEventListener("click", event => {
+        //reassign the text inside each element in the grid to currentPlayer
+        event.target.innerText=currentPlayer;
+        //if the current player is X and change to an O else change currently current player to X.
+        if(currentPlayer ==="X")
+        {currentPlayer = "O"}
+        else{currentPlayer ="X"}
+    })
+
+}
+
+
+
+
+
