acb7a7c0816e1d5cf5ea45b4103b11ee6815da52
Nayan Gurung <nayangrg.se@gmail.com>
2022-03-11 12:38:09 +0000

    restart button function

--- app.js
+++ app.js
@@ -1,18 +1,60 @@
 
-//Create an empty 2D array
 const gridEntries=Array(3)
-for (i = 0; i < 3; i++){
-    gridEntries[i] = Array(3)
+    for (i = 0; i < 3; i++){
+        gridEntries[i] = Array(3)
+    }
+    //get the grid element
+    const gridElements = document.getElementsByClassName("box");
+
+function start(){
+    
+    let currentPlayer= "X";
+    //loop through each box, avoid writing out each box
+    for (i = 0; i < gridElements.length; i++){
+        //each box is the element in the array
+       let box = gridElements[i];
+       //event listener is listening out for the user to click on each box and 
+        box.addEventListener("click", event => {
+            //reassign the text inside each element in the grid to currentPlayer
+    
+            if(event.target.innerText!="O" && event.target.innerText!="X"){
+                event.target.innerText=currentPlayer;
+                let index = event.target.id.split(",")  //Obtains index from the html id 
+                gridEntries[parseInt(index[0])][parseInt(index[1])]= currentPlayer
+    
+                //This calls the function to check whether the user won 
+                if(checkWin()){
+                    const h3id= document.getElementById("h3id"); 
+                    h3id.innerHTML= `Game over- ${playerName} won`
+                } else {
+                    if (checkDraw()) {
+                        console.log("stalemate")
+                        const h3id= document.getElementById("h3id"); 
+                        h3id.innerHTML= `Stalemate`;
+                    };
+                }
+    
+    
+                if(currentPlayer ==="X" || playerName === document.getElementById("PlayerOneName").value){
+                    currentPlayer = "O"
+                    playerName=document.getElementById("PlayerTwoName").value;
+                    document.getElementById("turn").innerText = playerName;
+                    
+                } else {
+                    currentPlayer ="X";
+                    playerName = document.getElementById("PlayerOneName").value
+                    document.getElementById("turn").innerText = playerName;
+                }
+        
+            }
+            //if the current player is X and change to an O else change currently current player to X.
+            
+        })
+    
+    }
 }
- 
-//get the grid element
-const gridElements = document.getElementsByClassName("box");
 
 
-let currentPlayer= "X";
-
-
-let playerName= document.getElementById("PlayerOneName").value;
 
 function checkWin(){
     
@@ -93,50 +135,8 @@ const checkDraw = function(){
     return result;
 }
 
-//loop through each box, avoid writing out each box
-for (i = 0; i < gridElements.length; i++){
-    //each box is the element in the array
-   let box = gridElements[i];
-   //event listener is listening out for the user to click on each box and 
-    box.addEventListener("click", event => {
-        //reassign the text inside each element in the grid to currentPlayer
 
-        if(event.target.innerText!="O" && event.target.innerText!="X"){
-            event.target.innerText=currentPlayer;
-            let index = event.target.id.split(",")  //Obtains index from the html id 
-            gridEntries[parseInt(index[0])][parseInt(index[1])]= currentPlayer
-
-            //This calls the function to check whether the user won 
-            if(checkWin()){
-                const h3id= document.getElementById("h3id"); 
-                h3id.innerHTML= `Game over- ${playerName} won`
-            } else {
-                if (checkDraw()) {
-                    console.log("stalemate")
-                    const h3id= document.getElementById("h3id"); 
-                    h3id.innerHTML= `Stalemate`;
-                };
-            }
-
-
-            if(currentPlayer ==="X" || playerName === document.getElementById("PlayerOneName").value){
-                currentPlayer = "O"
-                playerName=document.getElementById("PlayerTwoName").value;
-                document.getElementById("turn").innerText = playerName;
-                
-            } else {
-                currentPlayer ="X";
-                playerName = document.getElementById("PlayerOneName").value
-                document.getElementById("turn").innerText = playerName;
-            }
-    
-        }
-        //if the current player is X and change to an O else change currently current player to X.
-        
-    })
-
-}
-console.log(gridEntries)
+// console.log(gridEntries)
 
 
 function Player(){
@@ -147,6 +147,7 @@ function Player(){
     const playerTwo = document.getElementById("PlayerTwoName").value;
     console.log(playerOne);
     console.log(playerTwo);
+    start()
 
     // if (playerName === document.getElementById("PlayerOneName").value ){
     //     playerName=document.getElementById("PlayerTwoName").value;
@@ -155,6 +156,12 @@ function Player(){
     //     playerName = document.getElementById("PlayerOneName").value
     //     document.getElementById("turn").innerText = playerName;
     // }
+}
+
+
+function restart(){
+    start();
+    
 }
 
 
